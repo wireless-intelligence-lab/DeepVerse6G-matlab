0b10bd95c1fe3984d6d669da664df68bb9176335
Shuaifeng Jiang <jshuaifeng.ece@gmail.com>
2025-02-26 02:18:01 -0700

    Read scenario config from config.m
    Read scenario config from config.m instead of combined_param.mat

--- scripts/radar/construct_radar_signal.m
+++ scripts/radar/construct_radar_signal.m
@@ -5,6 +5,7 @@
 % ---------------------------------------------------------------------- %
 
 
+
 function [IF_signal] = construct_radar_signal(tx_ant_size, tx_rotation, tx_FoV, tx_ant_spacing, rx_ant_size, rx_rotation, rx_FoV, rx_ant_spacing, path_params, params)
 
     Fs = params.Fs;
@@ -69,7 +70,16 @@ function [IF_signal] = construct_radar_signal(tx_ant_size, tx_rotation, tx_FoV, 
         channel_LoS_status = -1;
     end
     path_FoV_filter = FoV;
-    power(~path_FoV_filter) = 0;
+    
+    num_paths = sum(path_FoV_filter);
+    power = power(path_FoV_filter);
+    Doppler_acc = path_params.Doppler_acc(path_FoV_filter);
+    Doppler_vel = path_params.Doppler_vel(path_FoV_filter);
+    phase = path_params.phase(path_FoV_filter);
+    delay_normalized = delay_normalized(path_FoV_filter);
+    array_response_TX = array_response_TX(:, path_FoV_filter);
+    array_response_RX = array_response_RX(:, path_FoV_filter);
+
 
     % Received signal calculation
     IF_sampling_mat = zeros(N_samples,num_paths); IF_sampling_mat2= IF_sampling_mat;
@@ -83,11 +93,11 @@ function [IF_signal] = construct_radar_signal(tx_ant_size, tx_rotation, tx_FoV, 
 
     if ismember(params.comp_speed, [5, 4, 3]) % Methods 3-5
         time_slow = time_fast + reshape(((0:1:(N_chirp-1))*T_PRI),1,1,[]);
-        Tau3_rt = ((double(path_params.Doppler_acc).*(time_slow.^2))./(2*physconst('LightSpeed')));
-        Tau2_rt = ((double(path_params.Doppler_vel).*time_slow)./physconst('LightSpeed'));
+        Tau3_rt = ((double(Doppler_acc).*(time_slow.^2))./(2*physconst('LightSpeed')));
+        Tau2_rt = ((double(Doppler_vel).*time_slow)./physconst('LightSpeed'));
         Tau_rt = (double(delay_normalized).*Ts) + Tau2_rt + Tau3_rt;
         %----- (a) additional traveling distance and (b) Doppler frequency affecting the phase terms
-        Extra_phase = exp(sqrt(-1)*double(path_params.phase).*ang_conv);
+        Extra_phase = exp(sqrt(-1)*double(phase).*ang_conv);
         Phase_terms  = exp(sqrt(-1)*2*pi*( (F0_active.*(Tau_rt)) -(0.5.*params.S.*(Tau_rt.^2)) +(params.S.*time_fast.*Tau_rt)));
         Phase_terms2 = exp(sqrt(-1)*2*pi*( (F0_active.*(Tau_rt-T_PRI)) -(0.5.*params.S.*((Tau_rt-T_PRI).^2)) +(params.S.*time_fast.*(Tau_rt-T_PRI)))); %Phase_Terms for the case of interchirp interference
         IF_mat = sqrt(double(power)).*conj(Extra_phase).*((Phase_terms.*IF_sampling_mat)+(Phase_terms2.*IF_sampling_mat2)); %%%% conjugate is based on the derivation we have reached for +sin(.) Quadrature signal as the input to the second mixer.
@@ -138,3 +148,4 @@ function [IF_signal] = construct_radar_signal(tx_ant_size, tx_rotation, tx_FoV, 
         end
     end
 end
+
