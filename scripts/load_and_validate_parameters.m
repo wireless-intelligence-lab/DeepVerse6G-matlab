dfafa69866908c5e676dea130d0e98f354527cb1
Shuaifeng Jiang <50979532+acyiobs@users.noreply.github.com>
2025-02-26 19:50:19 -0700

    Cleaning

--- scripts/load_and_validate_parameters.m
+++ scripts/load_and_validate_parameters.m
@@ -1,4 +1,4 @@
 function [full_data, params] = load_and_validate_parameters(params)
-    scenario_yaml_path = fullfile(params.dataset_folder, params.scenario, 'scenario.yaml');
+    scenario_yaml_path = fullfile(params.dataset_folder, params.scenario, 'param', 'scenario.yaml');
     full_data = parse_scenario_yaml(scenario_yaml_path);
 end
